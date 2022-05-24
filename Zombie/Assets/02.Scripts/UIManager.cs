99f5bd9216be2a60707805d41ca55867e6f2867e
gyullo18 <gold7098@naver.com>
2022-05-24 16:32:53 +0900

    20220524_2
    2권 책 ~p.687 초반

---
+++ Zombie/Assets/02.Scripts/UIManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+    
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this; // this = 내 자신
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void UpdateAmmoTEXT(int magAmmo, int ammoRemain)
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
